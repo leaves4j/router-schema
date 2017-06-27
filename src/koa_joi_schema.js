f84ab0c4bca45c5f7704f8a858952869c6d76f4f
jiangq <jiangq.leaves@gmail.com>
2017-06-27 16:54:15 +0800

    add document

--- src/koa_joi_schema.js
+++ src/koa_joi_schema.js
@@ -5,24 +5,39 @@ import type { JoiSchemaOption, ValidateData } from './type';
 
 
 const JoiSchema = require('./joi_schema');
-
+/**
+ * ExpressJoiSchema
+ *
+ * @class ExpressJoiSchema
+ * @extends {JoiSchema}
+ */
 class ExpressJoiSchema extends JoiSchema {
   constructor(option: JoiSchemaOption) {
     super(option);
   }
+
+ /**
+   * Implement JoiSchema.handler()
+   *
+   * @override
+   * @param {Context} ctx
+   * @param {() => Promise<void>} next
+   * @returns {ValidateData}
+   * @memberof ExpressJoiSchema
+   */
   handler(ctx: Context, next: () => Promise<void>): ValidateData {
     if (this.options.handler) {
       return this.options.handler(ctx, next);
     }
     const { query } = ctx;
-    const { body } = ctx.state.body;
+    const { body } = ctx.request;
     const callback = (error: null | Object, value: Object | null) => {
       if (error) {
-        throw error;
+        return Promise.reject(error);
       }
       if (value) {
         ctx.query = value.query;
-        ctx.body = value.body;
+        ctx.request.body = value.body;
       }
 
       return next();
